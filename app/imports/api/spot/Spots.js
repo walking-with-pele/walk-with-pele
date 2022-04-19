040350d245fd14246214b697c8f6133d90ed2549
joshrico <jrico@hawaii.edu>
2022-04-19 11:09:12 -1000

    Implemented "likes" into collection

--- app/imports/api/spot/Spots.js
+++ app/imports/api/spot/Spots.js
@@ -21,6 +21,7 @@ class SpotsCollection {
         allowedValues: ['beach', 'hike', 'library'],
         defaultValue: 'good',
       },
+      likes: Number,
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
