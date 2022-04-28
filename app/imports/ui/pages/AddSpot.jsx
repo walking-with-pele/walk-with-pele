0836231ed5ed9bf4c3687e38a763232eed1de214
joshrico <jrico@hawaii.edu>
2022-04-27 16:01:30 -1000

    Updated Accounts
    -included firstName and lastName into Accounts collection
    -in turn also modified the .username to include both firstName and lastName

--- app/imports/ui/pages/AddSpot.jsx
+++ app/imports/ui/pages/AddSpot.jsx
@@ -5,7 +5,7 @@ import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
-import { Comments } from '../../api/comment/Comments';
+import { Spots } from '../../api/spot/Spots';
 
 // Create a schema to specify the structure of the data to appear in the form.
 const formSchema = new SimpleSchema({
@@ -27,7 +27,7 @@ class AddSpot extends React.Component {
   submit(data, formRef) {
     const { name, address, spotType } = data;
     const owner = Meteor.user().username;
-    Comments.collection.insert({ name, address, spotType, owner },
+    Spots.collection.insert({ name, address, spotType, owner },
       (error) => {
         if (error) {
           swal('Error', error.message, 'error');
